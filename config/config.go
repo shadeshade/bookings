41b2cd8126656335fb163d642b84522839247866
abdulmagomedov <privetabdul@gmail.com>
2023-08-05 19:22:16 +0300

    Add config.

---
+++ config/config.go
@@ -0,0 +1,9 @@
+package config
+
+import (
+	"log"
+)
+
+type AppConfig struct {
+	InfoLog *log.Logger
+}
--- main.go
+++ main.go
@@ -3,10 +3,27 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/shadeshade/bookings-api/config"
 	"net/http"
 )
 
 const portNumber = ":8080"
+
+var Repo *Repository
+
+type Repository struct {
+	App *config.AppConfig
+}
+
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+func NewHandlers(r *Repository) {
+	Repo = r
+}
 
 type Hotel struct {
 	Title   string `json:"title"`
@@ -16,7 +33,7 @@ type Hotel struct {
 
 type Hotels []Hotel
 
-func allHotels(w http.ResponseWriter, req *http.Request) {
+func (m *Repository) allHotels(w http.ResponseWriter, req *http.Request) {
 	hotels := Hotels{
 		Hotel{
 			Title:   "Test title",
@@ -27,13 +44,18 @@ func allHotels(w http.ResponseWriter, req *http.Request) {
 	json.NewEncoder(w).Encode(hotels)
 }
 
-func Home(w http.ResponseWriter, req *http.Request) {
+func (m *Repository) Home(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "Homepage endpoint")
 }
 
 func main() {
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/hotels", allHotels)
+	var app config.AppConfig
+
+	repo := NewRepo(&app)
+	NewHandlers(repo)
+
+	http.HandleFunc("/", Repo.Home)
+	http.HandleFunc("/hotels", Repo.allHotels)
 
 	fmt.Println(fmt.Sprintf("Listening on port %s", portNumber))
 	_ = http.ListenAndServe(portNumber, nil)
