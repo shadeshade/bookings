71a10d781cf7b4e7358793f22f3e30f9ff9cafc8
abdulmagomedov <privetabdul@gmail.com>
2023-08-05 16:15:47 +0300

    Add test endpoints, add gitignore.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/shadeshade/bookings-api
+
+go 1.20
--- main.go
+++ main.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
+const portNumber = ":8080"
+
+type Hotel struct {
+	Title   string `json:"title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+type Hotels []Hotel
+
+func allHotels(w http.ResponseWriter, req *http.Request) {
+	hotels := Hotels{
+		Hotel{
+			Title:   "Test title",
+			Desc:    "Test description",
+			Content: "Hello World",
+		},
+	}
+	json.NewEncoder(w).Encode(hotels)
+}
+
+func Home(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "Homepage endpoint")
+}
+
+func main() {
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/hotels", allHotels)
+
+	fmt.Println(fmt.Sprintf("Listening on port %s", portNumber))
+	_ = http.ListenAndServe(portNumber, nil)
 }
